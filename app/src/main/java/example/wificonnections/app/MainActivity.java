be8b75472f874629e54e5bebb85b09306f372af3
Hardik Lodhiya <lodhiya_hardik@yahoo.com>
2014-07-25 19:51:47 -0500

    Android Project
    Hardik Lodhiya

---
+++ app/src/main/java/example/wificonnections/app/MainActivity.java
@@ -0,0 +1,143 @@
+package example.wificonnections.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+
+import java.util.List;
+//import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+//import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    TextView mainText;
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+    StringBuilder sb = new StringBuilder();
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        mainText = (TextView) findViewById(R.id.mainText);
+
+        // Initiate wifi service manager
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        // Check for wifi is disabled
+        if (mainWifi.isWifiEnabled() == false)
+        {
+            // If wifi disabled then enable it
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled",
+                    Toast.LENGTH_LONG).show();
+
+            mainWifi.setWifiEnabled(true);
+        }
+
+        // wifi scaned value broadcast receiver
+        receiverWifi = new WifiReceiver();
+
+        // Register broadcast receiver
+        // Broacast receiver will automatically call when number of wifi connections changed
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mainWifi.startScan();
+        mainText.setText("Starting Scan...");
+        mainText.append("Starting Scan.......!!!");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Refresh");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        mainWifi.startScan();
+        mainText.setText("Starting Scan");
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    protected void onPause() {
+        unregisterReceiver(receiverWifi);
+        super.onPause();
+    }
+
+    protected void onResume() {
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    // Broadcast receiver class called its receive method
+    // when number of wifi connections changed
+
+    class WifiReceiver extends BroadcastReceiver {
+
+        // This method call when number of wifi connections changed
+        public void onReceive(Context c, Intent intent) {
+
+            sb = new StringBuilder();
+            wifiList = mainWifi.getScanResults();
+            sb.append("\n        Number Of Wifi connections :"+wifiList.size()+"\n\n");
+
+            for(int i = 0; i < wifiList.size(); i++){
+
+                sb.append(new Integer(i+1).toString() + ". ");
+                sb.append((wifiList.get(i)).toString());
+                sb.append("\n\n");
+            }
+
+            mainText.setText(sb);
+        }
+
+    }
+}
+
+
+
+
+
+/*
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+*/
